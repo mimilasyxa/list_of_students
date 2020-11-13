9a7c33eec12ab126cc1b8a767ca492ee07bfc815
mimilasyxa <50004735+mimilasyxa@users.noreply.github.com>
2020-11-13 18:56:16 +0700

    upd -

--- list_of_students/list_of_students/Form1.cs
+++ list_of_students/list_of_students/Form1.cs
@@ -14,7 +14,7 @@ namespace list_of_students
     public partial class Form1 : Form
     {
         Random rand = new Random();
-        public static string[] lname =  {"Смит", "Вэй", "Мюллер", "Дламини", "Сильва", "Сингх"};
+        public static string[] lname = { "Смит", "Вэй", "Мюллер", "Дламини", "Сильва", "Сингх" };
         public static string[] fname = { "Алекс", "Кортни", "Тейлор", "Медисон", "Пейдж", "Эрин" };
         public static string[] mname = { "Александровна", "Никитович", "Матвеевич", "Михайловна", "Денисович", "Романович" };
         public Form1()
@@ -22,7 +22,7 @@ namespace list_of_students
             InitializeComponent();
         }
         //  string sql = string.Format("Insert Into group" +
-                   //"(Lname, Fname, Mname, avg_score ,original_docs, budget) Values('{0}','{1}','{2}',{3},{4},{5})", textBox1.Text, textBox2.Text, textBox3.Text, Convert.ToDouble(textBox4.Text), Convert.ToInt32(checkBox1.Checked), Convert.ToInt32(checkBox2.Checked));
+        //"(Lname, Fname, Mname, avg_score ,original_docs, budget) Values('{0}','{1}','{2}',{3},{4},{5})", textBox1.Text, textBox2.Text, textBox3.Text, Convert.ToDouble(textBox4.Text), Convert.ToInt32(checkBox1.Checked), Convert.ToInt32(checkBox2.Checked));
         private void button1_Click(object sender, EventArgs e)
         {
             string Connect = "server=localhost;port=3307;username=root;password=root;database=students";
@@ -40,13 +40,14 @@ namespace list_of_students
 
             con.Close();
         }
-        private string GetString(string type) {
+        private string GetString(string type)
+        {
             if (type == "lname")
             {
                 return lname[rand.Next(6)];
             }
             if (type == "fname")
-            {   
+            {
                 return fname[rand.Next(6)];
             }
             if (type == "mname")
@@ -67,9 +68,7 @@ namespace list_of_students
                 textBox2.Text = GetString("fname");
                 textBox3.Text = GetString("mname");
                 textBox4.Text = (Math.Round(rand.NextDouble() * 5, 1)).ToString();
-                return true;     
+                return true;
             }
             return base.ProcessCmdKey(ref msg, keyData);
         }
-    }
-}
