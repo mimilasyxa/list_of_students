d3894fa26506b3171c8479c43e5bda5b3847e63a
insomnla <Epikmen@bk.ru>
2020-11-22 16:09:53 +0700

    deleted table "groups", added new table "name_specialty" :+1:

--- DB/sql requests.sql
+++ DB/sql requests.sql
@@ -1,5 +1,5 @@
 use students;
-select * from students;
-select * from groups;
+select * from student;
+select * from name_specialty;
 select * from original_documents;
-select id_students as '№', lname as 'Фамилия', fname as 'Имя', mname as 'Отчество', average_score as 'Средний бал', original_documents as 'Оригинальные документы', budget as 'Бюджет', groups.group as 'Группа' from students, groups, original_documents where students.fk_id_groups = groups.id_groups and students.fk_id_original_documents = original_documents.id_original_documents and id_students < 25 order by average_score desc, fk_id_original_documents asc;
+select id_student as '№', lname as 'Фамилия', fname as 'Имя', mname as 'Отчество', average_score as 'Средний бал', original_documents as 'Оригинальные документы', budget as 'Бюджет', name_specialty as 'Наименование специальности', specialty_code as 'Код специальности' from student, name_specialty, original_documents where student.fk_id_name_specialty = name_specialty.id_name_specialty and student.fk_id_original_documents = original_documents.id_original_documents and id_student < 25 order by average_score desc, fk_id_original_documents asc;
